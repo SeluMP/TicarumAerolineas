afa77a1ca5dad276a79e5e91ae64581030ef0867
SeluMP <joseluone@gmail.com>
2023-03-11 09:53:38 +0100

    Commit inicial - SpringBoot Base y dependencias

---
+++ ticarum-aerolineas-api/src/main/java/com/ticarum/aerolineas/ticarumaerolineasapi/TicarumAerolineasApiApplication.java
@@ -0,0 +1,13 @@
+package com.ticarum.aerolineas.ticarumaerolineasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicarumAerolineasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicarumAerolineasApiApplication.class, args);
+	}
+
+}
---
+++ ticarum-aerolineas-api/src/test/java/com/ticarum/aerolineas/ticarumaerolineasapi/TicarumAerolineasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticarum.aerolineas.ticarumaerolineasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicarumAerolineasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
